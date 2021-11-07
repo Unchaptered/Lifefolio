560b481ed8e066cc39a9587fae60329f2282a83d
Unchaptered <workstation19961002@gmail.com>
2021-11-07 23:53:21 +0900

    Router

--- src/router/folioRouter.js
+++ src/router/folioRouter.js
@@ -7,6 +7,8 @@ import { uploadPost } from "../controller/folioControllers";
 import { folioViewGet } from "../controller/folioViewControllers";
 import { folioViewPost } from "../controller/folioViewControllers";
 
+import { folioLike, folioImageLike, folioImageShare, folioImageFollow, folioImageFind } from "../controller/folioLikeController";
+
 import { loginUserMD, upFolioShortcutMiddleware, upFolioImagesMiddleware } from "../middleware";
 
 const folioRouter=express.Router();
@@ -23,9 +25,28 @@ folioRouter
     .post(upFolioShortcutMiddleware.single("shortcut"),uploadPost);
 
 folioRouter
-    .route("/:id")
+    .route("/:folioID")
     .get(folioViewGet)
     .all(loginUserMD)
     .post(upFolioImagesMiddleware.single("folioImage"),folioViewPost);
 
+// contains self middleware
+folioRouter
+    .route("/:folioID/like")
+    .get(folioLike);
+folioRouter
+    .route("/:folioID/:imageID/like")
+    .get(folioImageLike);
+
+folioRouter
+    .route("/:folioID/:imageID/share")
+    .get(folioImageShare);
+
+folioRouter
+    .route("/:folioID/:imageID/follow")
+    .get(folioImageFollow);
+
+folioRouter
+    .route("/image/:imageID/find")
+    .get(folioImageFind);
 export default folioRouter;
